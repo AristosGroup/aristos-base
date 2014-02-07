31c1ab996201fea5f6aac375465ecd6c018d61fd
Aleksey Kuznetsov <mrakobesov@gmail.com>
2014-02-07 12:55:58 +0400

    update user permissions

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,17 +1,4 @@
 Meteor.startup(function () {
-    if (Meteor.roles.find().count() == 0) {
-        Roles.createRole('admin');
-        Roles.createRole('manage');
-        Roles.createRole('view');
-        Roles.createRole('view-secret');
-    }
 
-
-    if (Meteor.users.findOne("gs6ujsRvAD2jS57mu"))
-        Roles.addUsersToRoles("gs6ujsRvAD2jS57mu", ['admin']);
-
-
-    if (Meteor.users.findOne("jQJGBNo4gDw6gDE8X"))
-        Roles.addUsersToRoles("jQJGBNo4gDw6gDE8X", ['admin']);
 
 });
