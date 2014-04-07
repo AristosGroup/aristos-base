f318d41a7a6d12cf4ebdb55b12f19896e743cefd
ProWebCraft <prowebcraft@gmail.com>
2014-04-07 12:03:31 +0400

    Отладка главного меню и авторизации

--- client/aside/mainMenu.js
+++ client/aside/mainMenu.js
@@ -3,6 +3,7 @@ Template.mainMenu.helpers(
         mainMenu: function () {
             //var menuCount = Session.get('menuItems');
             //AristosUtils.debug('Количество пунктов меню:', menuCount);
+            App.menuDep.depend(); //Зависимость от смены пунктов меню
             var menu = App.getMainMenu();
             menu = _.map(menu, function (item) {
                 if (Router.current() && _(Router.current().route.name).startsWith(item.key))
